59210eb0a8b70acdc0477eb43fee8bdc9caf6742
Rapeephat01 <watdee00@GMAIL.COM>
2024-12-20 14:43:39 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>   
+using namespace std;
+
+int main() {
+    
+    srand(static_cast<unsigned int>(time(0)));
+
+  
+    const char* grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    const int total_grades = 9;
+
+    
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    
+    for (int i = 0; i < 3; ++i) {
+        cin.ignore(); 
+    }
+
+    
+    int random_index = rand() % total_grades;
+
+    
+    cout << "You will get " << grades[random_index] << " in this 261102." << endl;
+
+    return 0;
+}
